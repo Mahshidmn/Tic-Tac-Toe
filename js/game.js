914757ed52b7c7cea5a8e70a81d4d5f60bced424
mahshidmn <ma.mojtabavi@gmail.com>
2024-02-06 12:25:47 +1100

    second commit

--- js/game.js
+++ js/game.js
@@ -1,49 +1,61 @@
 
 /*----- state variables -----*/
+
 const state = {
     board: null,
     player: 'x'
+
 };
 
 
 /*----- cached elements  -----*/
 const squares = document.querySelectorAll('.square');
+const resetBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
-squares.forEach(function(square) {
-    square.addEventListener('click', placeTile);
-});
 
+// loop through squares and listen for a click on each
+squares.forEach(function(square) {
+  square.addEventListener('click', placeTile);
+});
+resetBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init();
 
 function init() {
-    state.board = {
-        'top-left': null, 'top-center': null, 'top-right': null,
-        'center-left': null, 'center-center': null, 'center-right': null,
-        'bottom-left': null, 'bottom-center': null, 'bottom-right': null,
-    };
-    state.player = 'x';
-    render();
-        
-    }
-
+state.board = {
+    'top-left': null, 'top-center': null, 'top-right': null,
+    'center-left': null, 'center-center': null, 'center-right': null,
+    'bottom-left': null, 'bottom-center': null, 'bottom-right': null,
+};
+state.player = 'x';
+render();
+}
 
 function render() {
-    for (let id in state.board) {
-        document.getElementById(id).innerText = state.board[id];
-    }
+for (let id in state.board) {
+  document.getElementById(id).innerText = state.board[id];
+}
 }
 
-function placeTile() {
+function placeTile () {
     const id = this.id;
-    //console.log('the click tile was', this);
-    if (state.board[id]) return;
+
+if (state.board[id]) return; //exit the function if this tile already has been occupied
+
     state.board[id] = state.player;
-    state.player = state.player === 'x' ? 'o' : 'x';    
+
+   /// if (state.player === 'x') {
+     ///   state.player = 'o';
+   // } else {
+       // state.player = 'x';
+   /// }
+   state.player = state.player === 'x' ? 'o' : 'x';// switching players 
 
     render();
+    
 }
 
-//Todo: add a reset button that clears the content of the page
+
+
